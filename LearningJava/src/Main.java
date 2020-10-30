531a0a7aaef317bdb9d3306d7ea1f19cec6a040b
Alessandro Demela <alessandro.demela@gmail.com>
2020-10-30 11:18:17 +0100

    Concatenating strings

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -8,12 +8,6 @@ public class Main {
         char studentFirstInitial = studentFirstName.charAt(0);
         char studentLastInitial = studentLastName.charAt(0);
 
-        System.out.println(studentAge);
-        System.out.println(studentGPA);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
-        System.out.println(hasPerfectAttendance);
-        System.out.println(studentFirstName);
-        System.out.println(studentLastName);
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of "+ studentGPA);
     }
 }
