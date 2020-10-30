71a1763219b2c079085fe6a1facddccd2d47158c
Alessandro Demela <alessandro.demela@gmail.com>
2020-10-30 11:10:11 +0100

    Create class to represent a student

---
+++ LearningJava/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args){
+        int studentAge = 15;
+        double studentGPA = 3.42;
+        char studentFirstInitial = 'A';
+        char studentLastInitial = 'D';
+        boolean hasPerfectAttendance = true;
+        String studentFirstName = "Alessandro" ;
+        String studentLastName = "Demela";
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
+    }
+}
