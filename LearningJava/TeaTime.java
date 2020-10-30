21f3a242c2b35256cb84f4a4addc28727c92ad7c
Alessandro Demela <alessandro.demela@gmail.com>
2020-10-30 15:40:15 +0100

    First function

---
+++ LearningJava/TeaTime.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TeaTime {
+
+    public static void annouceDeveloperTeaTime() {
+        System.out.println("Waiting for developer tea time...");
+        System.out.println("Type a random word and then press Enter to announce tea time");
+
+        Scanner input = new Scanner(System.in);
+        input.next();
+
+        System.out.println("It's developer tea time! ");
+
+        scanner.close();
+    }
+
+   public static void main(String[] args) {
+       
+   }
+
+
+}
