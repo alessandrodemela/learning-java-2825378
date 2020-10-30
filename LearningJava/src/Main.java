0ae6dacdb0669ba9a84b649e3595ded7909e0fad
Alessandro Demela <alessandro.demela@gmail.com>
2020-10-30 11:15:58 +0100

    Using charAt

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args){
         int studentAge = 15;
         double studentGPA = 3.42;
-        char studentFirstInitial = 'A';
-        char studentLastInitial = 'D';
         boolean hasPerfectAttendance = true;
         String studentFirstName = "Alessandro" ;
         String studentLastName = "Demela";
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
