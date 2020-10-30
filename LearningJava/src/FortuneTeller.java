31148534abf6506892329d2a07adb7685fa15192
Alessandro Demela <alessandro.demela@gmail.com>
2020-10-30 12:02:13 +0100

    FortuneTeller complete

--- LearningJava/src/FortuneTeller.java
+++ LearningJava/src/FortuneTeller.java
@@ -1,5 +1,16 @@
+import java.util.*;
+
 public class FortuneTeller {
     public static void main(String[] args){
-        
+        System.out.println("Which number from 1 to 10 you want to pick?");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        int inputNum = scanner.nextInt();
+
+        if (inputNum<5) {
+            System.out.println("Good luck!");
+        } else {
+            System.out.println("Bad luck!");
+        }
     }
 }
