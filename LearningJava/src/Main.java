d9d5dffa0eee710f479ebc95e967edfaecb18ab1
Alessandro Demela <alessandro.demela@gmail.com>
2020-11-02 12:05:38 +0100

    Triangle class

---
+++ LearningJava/src/Main.java
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////////////////////////////
+//
+//      Author: Alessandro Demela
+//      Date: 30/10/2020
+//      Main class for Triangle.java
+//
+//////////////////////////////////////////////////////////////////////
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
+        Triangle triangleB = new Triangle(3, 2.598, 3, 3, 3);
+
+        double triangeAArea = triangleA.findArea();
+        System.out.println(triangeAArea);
+
+    }
+    
+}
---
+++ LearningJava/src/Triangle.java
@@ -0,0 +1,23 @@
+//////////////////////////////////////////////////////////////////////
+//
+//      Author: Alessandro Demela
+//      Date: 30/10/2020
+//      Class to describe a triangle
+//
+//////////////////////////////////////////////////////////////////////
+
+public class Triangle {
+    double base, height, sideLengthOne, sideLengthTwo, sideLengthThree;
+
+    public Triangle(double base, double height, double sideLengthOne, double sideLengthTwo, double sideLengthThree){
+        this.base = base;
+        this.height = height;
+        this.sideLengthOne = sideLengthOne;
+        this.sideLengthTwo = sideLengthTwo;
+        this.sideLengthThree = sideLengthThree;
+    }
+
+    public double findArea() {
+        return this.base * this.height / 2;
+    }
+}
